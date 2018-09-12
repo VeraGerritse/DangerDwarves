c24d57dd61d3edfddd4799f019f648daa55207fa
VeraGerritseDeltion <arev_97@live.nl>
2018-09-12 11:10:14 +0200

    stats and items

---
+++ Y3P1/Assets/Scripts/Vera/Item.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(menuName = "Custom Objects/Items")]
+public class Item : ScriptableObject
+{
+    [SerializeField] private string itemName;
+    [SerializeField] private enum ItemRarity {common, rare, epic,legendary}
+    [SerializeField] private ItemRarity itemRarity;
+    [SerializeField] private Sprite itemImage;
+    [SerializeField] private Stats myStats;
+}
