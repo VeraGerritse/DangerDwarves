3b7344e94d2061546fb413d25569aaec3988de41
Dominik Beens <dominik.beens@hotmail.nl>
2018-09-17 18:31:33 +0200

    added notification system

--- Y3P1/Assets/Scripts/Dominik/Notifications/Notification.cs
+++ Y3P1/Assets/Scripts/Dominik/Notifications/Notification.cs
@@ -1,22 +1,11 @@
 ﻿using UnityEngine;
 using TMPro;
-using UnityEngine.UI;
 
 public class Notification : MonoBehaviour 
 {
 
-    private Color newColor;
-
     [SerializeField] private float moveSpeed = 1f;
-    [SerializeField] private float fadeSpeed = 2f;
-    [SerializeField] private Image notificationBackground;
     [SerializeField] private TextMeshProUGUI notificationText;
-
-    private void OnEnable()
-    {
-        notificationText.color = Color.white;
-        newColor = notificationText.color;
-    }
 
     public void Initialise(string text)
     {
@@ -27,13 +16,7 @@ public class Notification : MonoBehaviour
     {
         transform.Translate(Vector3.up * moveSpeed);
 
-        if (notificationText.color.a > 0.1f)
-        {
-            newColor.a -= Time.deltaTime * fadeSpeed;
-            notificationText.color = newColor;
-            notificationBackground.color = newColor;
-        }
-        else
+        if (notificationText.color.a < 0.05f)
         {
             ObjectPooler.instance.AddToPool("Notification", gameObject);
         }
