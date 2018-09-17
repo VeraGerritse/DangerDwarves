dc395efeac0c7fdd2831488720f0d79021e38744
VeraGerritseDeltion <arev_97@live.nl>
2018-09-17 15:36:56 +0200

    start inventory

---
+++ Y3P1/Assets/Scripts/Vera/InventorySlot.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Y3P1;
+
+public class InventorySlot : MonoBehaviour {
+    [SerializeField] private Inventory inventory;
+    public enum SlotType {all,weapon,armor,nothing}
+    public SlotType slotType;
+    [SerializeField] private Image myItem;
+    
+    public bool CheckSlotType()
+    {
+        if(slotType == SlotType.all)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public void EquipWeapon(Weapon toEquip)
+    {
+        if(Player.localPlayer != null)
+        {
+            Player.localPlayer.weaponSlot.EquipWeapon(toEquip);
+        }
+    }
+
+    public void SetImage(Sprite mySprite)
+    {
+        myItem.sprite = mySprite;
+    }
+    
+    public void EnableImage()
+    {
+        myItem.enabled = true;
+    }
+
+    public void DisableImage()
+    {
+        myItem.enabled = false;
+    }
+
+    private void Awake()
+    {
+        inventory = GetComponentInParent<Inventory>();
+        if(inventory == null)
+        {
+            Debug.Log("No inventory Found");
+            Destroy(gameObject);
+            return;
+        }
+
+        //myItem = GetComponentInChildren<Image>();
+        //myItem.enabled = false;
+        inventory.AddSlots();
+    }
+
+    public void OnMouseEnter()
+    {
+        inventory.SetCurrentSlot(this);
+    }
+
+    public void OnDrag()
+    {
+        inventory.StartDragging();
+        myItem.color = new Color(255, 255, 255, 0.25f);
+        print(myItem.color);
+    }
+
+    public void OnDrop()
+    {
+        inventory.StopDragging();
+        myItem.color = new Color(255, 255, 255, 1);
+    }
+
+    public void Dragging()
+    {
+
+    }
+    public void OnMouseExit()
+    {
+        if (!inventory.IsDragging())
+        {
+            inventory.SetLastSlot(this);
+        }
+    }
+}
---
+++ Y3P1/Assets/Scripts/Vera/LootRandomizer.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LootRandomizer : MonoBehaviour {
+    public List<Item> test = new List<Item>();
+    public int testItemLevel;
+    public static LootRandomizer instance;
+
+
+    [SerializeField] private int commonPercent;
+    [SerializeField] private int rarePercent;
+    [SerializeField] private int epicPercent;
+    //test shiz
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            for (int i = 0; i < 1000; i++)
+            {
+                DropLoot();
+            }
+        }
+    }
+
+
+    public Item DropLoot()
+    {
+        Item testItem = ScriptableObject.CreateInstance<Weapon_Ranged>();
+        testItem.StartUp(Database.hostInstance.GetArmorName(), Rarity(), Database.hostInstance.GetSpriteCrossbow());
+        test.Add(testItem);
+        return testItem;
+    }
+
+    private int Rarity()
+    {
+        int chance = 0;
+        chance = Random.Range(0, 100);
+
+        if (chance < commonPercent)
+        {
+            return 0;
+        }
+        if (chance < rarePercent)
+        {
+            return 1;
+        }
+        if (chance < epicPercent)
+        {
+            return 2;
+        }
+
+
+        return 3;
+    }
+}
