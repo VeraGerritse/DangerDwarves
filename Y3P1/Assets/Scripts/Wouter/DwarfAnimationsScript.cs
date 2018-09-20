ed4e15aa2bbe646dd97535fae261ed3af4aa285e
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-09-20 12:58:05 +0200

    Blend tree progress

---
+++ Y3P1/Assets/Scripts/Wouter/DwarfAnimationsScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DwarfAnimationsScript : MonoBehaviour {
+
+    public Animator myanim;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        myanim.SetFloat("HorizontalAxis", Input.GetAxis("Horizontal"));
+        myanim.SetFloat("VerticalAxis", Input.GetAxis("Vertical"));
+    }
+
+
+}
---
+++ Y3P1/Assets/Scripts/Wouter/HeadTracking.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadTracking : MonoBehaviour {
+
+    public GameObject headbone;
+    public Transform target;
+    public Quaternion mystuff;
+
+    
+
+    // Update is called once per frame
+    void LateUpdate () {
+
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 toOther = target.position - transform.position;
+
+
+        if (Vector3.Dot(forward, toOther) > 0)
+        {
+            Vector3 relativePos = target.position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePos);
+            headbone.transform.rotation = rotation;
+        }
+
+    }
+}
