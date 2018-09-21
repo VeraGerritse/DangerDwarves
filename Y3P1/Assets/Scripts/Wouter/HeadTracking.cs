fa994085758ff2fed848d059ab2185e6b824c80c
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-09-21 11:11:22 +0200

    bowtracking

--- Y3P1/Assets/Scripts/Wouter/HeadTracking.cs
+++ Y3P1/Assets/Scripts/Wouter/HeadTracking.cs
@@ -6,29 +6,48 @@ public class HeadTracking : MonoBehaviour {
 
     public GameObject headbone;
     public Transform target;
+    public Vector3 targetCords;
     public Quaternion mystuff;
 
-    
+    public bool gettarget;
+
+
+
+    private void Start()
+    {
+        //target = PlayerController.mouseInWorldPos;
+
+    }
+
+
 
     // Update is called once per frame
     void LateUpdate () {
 
+        if (gettarget)
+        {
+            targetCords = PlayerController.mouseInWorldPos;
+        }
+
         Vector3 forward = transform.TransformDirection(Vector3.forward);
-        Vector3 toOther = target.position - transform.position;
+        Vector3 toOther = targetCords - transform.position;
 
-
+        
         if (Vector3.Dot(forward, toOther) > 0)
         {
-            Vector3 relativePos = target.position - transform.position;
+            Vector3 relativePos = targetCords - transform.position;
             Quaternion rotation = Quaternion.LookRotation(relativePos);
             headbone.transform.rotation = rotation;
         }
         else
         {
-            //Vector3 relativePos = target.position - transform.position;
-            //Quaternion rotation = Quaternion.LookRotation(relativePos);
-            //headbone.transform.rotation = rotation;
+            /*
+            Vector3 relativePos = target.position - transform.position;
+            Quaternion rotation = Quaternion.LookRotation(relativePos);
+            headbone.transform.rotation = rotation;
+            */
         }
+        
 
     }
 }
