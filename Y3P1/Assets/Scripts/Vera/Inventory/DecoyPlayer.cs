b6f50f4f450a5e6aca1e6531d3b6c4509d01543e
VeraGerritseDeltion <arev_97@live.nl>
2018-10-08 16:29:42 +0200

    2nd added

---
+++ Y3P1/Assets/Scripts/Vera/Inventory/DecoyPlayer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DecoyPlayer : MonoBehaviour {
+
+    [SerializeField] private List<GameObject> beards = new List<GameObject>();
+    [SerializeField] private List<Material> beardMaterials = new List<Material>();
+    [SerializeField] private List<Material> skinMaterials = new List<Material>();
+
+    private GameObject currentBeard;
+
+    public Renderer skins;
+
+    public Camera faceCam;
+    public Camera bodyCam;
+
+    public void ChangeAppearance(int beard, int beardColor, int skin)
+    {
+        DisableBeard();
+        currentBeard = beards[beard];
+        currentBeard.SetActive(true);
+        currentBeard.GetComponent<Renderer>().material = beardMaterials[beardColor];
+        skins.material = skinMaterials[skin];
+    }
+
+    private void DisableBeard()
+    {
+        currentBeard.SetActive(false);
+    }
+}
