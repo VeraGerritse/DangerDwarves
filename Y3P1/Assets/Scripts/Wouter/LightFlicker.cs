7cae3708a62105da7202e22a31c8efc3a973a7df
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-10-10 15:38:33 +0200

    light flickr

---
+++ Y3P1/Assets/Scripts/Wouter/LightFlicker.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour
+{
+
+    private Light myLight;
+
+    public float minIntensity;
+    public float maxIntensity;
+
+    public float minRange;
+    public float maxRange;
+
+    private float random;
+
+    private void Awake()
+    {
+        myLight = GetComponent<Light>();
+    }
+
+    private void Start()
+    {
+        random = Random.Range(0, 1000);
+    }
+
+    private void Update()
+    {
+        float noise = Mathf.PerlinNoise(random, Time.time);
+        myLight.intensity = Mathf.Lerp(minIntensity, maxIntensity, noise);
+        myLight.range = Mathf.Lerp(minRange, maxRange, noise);
+    }
+}
