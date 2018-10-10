bd7b2b4d2c6864c04d81f981070ddc741f9e0ccf
Dominik Beens <dominik.beens@hotmail.nl>
2018-10-10 21:21:33 +0200

    changed animated arrow trajectory, implemented gold pile model

---
+++ Y3P1/Assets/Scripts/Dominik/NetworkedProp.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using Photon.Pun;
+
+public class NetworkedProp : MonoBehaviour 
+{
+
+    [SerializeField] private GameObject prop;
+    [SerializeField] private float gizmoRadius = 1;
+    [SerializeField] private Vector3 gizmoOffset;
+
+    private void Awake()
+    {
+        if (PhotonNetwork.IsMasterClient)
+        {
+            GameObject propSpawn = PhotonNetwork.InstantiateSceneObject(prop.name, transform.position, transform.rotation);
+            propSpawn.transform.SetParent(transform);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position + gizmoOffset, gizmoRadius);
+    }
+}
