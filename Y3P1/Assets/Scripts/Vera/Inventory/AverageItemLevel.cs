49cc84bc7149b22455f80a1f6883723a038a328c
VeraGerritseDeltion <arev_97@live.nl>
2018-10-17 10:11:12 +0200

    test for A.I.L.

--- Y3P1/Assets/Scripts/Vera/Inventory/AverageItemLevel.cs
+++ Y3P1/Assets/Scripts/Vera/Inventory/AverageItemLevel.cs
@@ -2,16 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Y3P1;
 
-public class AverageItemLevel : MonoBehaviour {
+public class AverageItemLevel : MonoBehaviourPunCallbacks {
+    public int averageILevel;
+    public int test;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            CalculateLevel();
+            NotificationManager.instance.NewNotification(test.ToString());
+        }
+    }
+
+    public void CalculateLevel()
+    {
+        photonView.RPC("GetLevel", RpcTarget.All);
+    }
+
+    [PunRPC]
+    private void GetLevel()
+    {
+        test += 1;
+    }
 }
