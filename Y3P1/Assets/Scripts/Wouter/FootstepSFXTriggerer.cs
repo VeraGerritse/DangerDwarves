3c8fdf387d215864f1e01a880097439c86b2fd95
GMD1A_WouterUjvari <wouter.ujvari@gmail.com>
2018-10-27 15:59:09 +0200

    Sounds, pls ignore wip footsteps

---
+++ Y3P1/Assets/Scripts/Wouter/FootstepSFXTriggerer.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootstepSFXTriggerer : MonoBehaviour {
+
+    public List<AudioSource> SFXFootstepsStone = new List<AudioSource>();
+    public List<AudioSource> SFXFootstepsWooden = new List<AudioSource>();
+    public List<AudioSource> SFXFootstepsSoil = new List<AudioSource>();
+
+
+    public void Update()
+    {
+        //PickAndPlaySound();
+    }
+    public void PickAndPlaySound()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.down), out hit, 1))
+        {
+            if(hit.transform.GetComponent<MaterialTagger>() != null)
+            {
+                if(hit.transform.GetComponent<MaterialTagger>().materialType == MaterialTagger.MaterialType.Stone)
+                {
+                    SFXFootstepsStone[Random.Range(0,SFXFootstepsStone.Count)].Play();
+                }
+                if (hit.transform.GetComponent<MaterialTagger>().materialType == MaterialTagger.MaterialType.Wooden)
+                {
+                    SFXFootstepsWooden[Random.Range(0, SFXFootstepsWooden.Count)].Play();
+                }
+                if (hit.transform.GetComponent<MaterialTagger>().materialType == MaterialTagger.MaterialType.Soil)
+                {
+                    SFXFootstepsSoil[Random.Range(0, SFXFootstepsSoil.Count)].Play();
+                }
+            }
+        }
+        
+    }
+}
---
+++ Y3P1/Assets/Scripts/Wouter/MaterialTagger.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MaterialTagger : MonoBehaviour {
+
+    public enum MaterialType { Stone, Wooden, Soil };
+    public MaterialType materialType;
+}
