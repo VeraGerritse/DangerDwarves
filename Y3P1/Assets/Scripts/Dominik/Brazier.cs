fa034616b3009656a202df882daa52460b840d5b
Dominik Beens <dominik.beens@hotmail.nl>
2018-10-29 20:23:26 +0100

    brazier prefab

---
+++ Y3P1/Assets/Scripts/Dominik/Brazier.cs
@@ -0,0 +1,22 @@
+﻿using Photon.Pun;
+using UnityEngine;
+
+public class Brazier : MonoBehaviourPunCallbacks
+{
+
+    [SerializeField] private GameObject particle;
+
+    public void Light()
+    {
+        if (!particle.activeInHierarchy)
+        {
+            photonView.RPC("LightBrazier", RpcTarget.AllBuffered);
+        }
+    }
+
+    [PunRPC]
+    private void LightBrazier()
+    {
+        particle.SetActive(true);
+    }
+}
